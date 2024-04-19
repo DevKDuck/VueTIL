08e501a66075ef8d06792de1ed784e76a01f508b
DevKDuck <loading95@naver.com>
2024-04-19 11:55:25 +0900

    toggle을 이용하여 바인딩

--- hello-app-vite/src/App.vue
+++ hello-app-vite/src/App.vue
@@ -1,4 +1,4 @@
-<template>
+<!-- <template>
   <img alt="Vue logo" src="./assets/logo.png" />
   <Hello/>
   <step1/>
@@ -8,3 +8,61 @@
   import Hello from '/@mycomp/Hello.vue'
   import step1 from '/@mycomp/step1.vue'
 </script>
+ -->
+
+
+<script setup>
+import { ref } from 'vue'
+
+const isActive = ref(true);
+const hasError = ref(false);
+const awesome = ref(true)
+
+function toggle() {
+  awesome.value = !awesome.value;
+  isActive.value = !isActive.value;
+  console.log("isActive", isActive);
+  // const aaa = document.querySelector("#aaa");
+  // if (isActive.value) {
+  //   aaa.classList.add("active");
+  // } else {
+  //   aaa.classList.remove("active");
+  // }
+  
+}
+function errorToggle() {
+  hasError.value = !hasError.value;
+  // const aaa = document.querySelector("#aaa");
+  // if (hasError.value) {
+  //   aaa.classList.add("text-danger");
+  // } else {
+  //   aaa.classList.remove("text-danger");
+  // }
+
+}
+</script>
+
+<template>
+  <button @click="toggle">토글 버튼</button>
+  <button @click="errorToggle">error 토글 버튼</button>
+  <h1 v-if="awesome">Vue는 굉장해! 엄청나!</h1>
+  <h1 v-else>오 안돼 😢</h1>
+  <!-- div id="aaa" class="static">만세~~~</div -->
+  <div class="static" :class="{active: isActive, 'text-danger' : hasError}">만세~~~</div>
+</template>
+
+<style >
+    .static {
+        border:4px solid black;
+        width:200px;
+        height:200px;
+    }
+    .active {
+        font-size: 50px;
+    }
+    .text-danger {
+        background-color: red; 
+        color:white;
+        font-size:100px;
+    }
+</style>
