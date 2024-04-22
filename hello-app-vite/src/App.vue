4aca8aab141c6237d57b668c6bc3d435f53fcc60
DevKDuck <loading95@naver.com>
2024-04-22 18:08:10 +0900

    LoginForm 이벤트핸들러를 이용하여 콘솔에 찍어보기

--- hello-app-vite/src/App.vue
+++ hello-app-vite/src/App.vue
@@ -1,49 +1,54 @@
 
+
 <script setup>
 import { ref, computed } from 'vue'
-import ComputedTodolist from '/@mycomp/ComputedTodolist.vue'
+import LoginForm from '/@mycomp/LoginForm.vue'
+// import ComputedTodolist from '/@mycomp/ComputedTodolist.vue'
 
-const isActive = ref(true);
-const hasError = ref(false);
-const awesome = ref(true)
 
-function toggle() {
-  awesome.value = !awesome.value;
-  isActive.value = !isActive.value;
-  console.log("isActive", isActive);
-  // const aaa = document.querySelector("#aaa");
-  // if (isActive.value) {
-  //   aaa.classList.add("active");
-  // } else {
-  //   aaa.classList.remove("active");
-  // }
+// const isActive = ref(true);
+// const hasError = ref(false);
+// const awesome = ref(true)
+
+// function toggle() {
+//   awesome.value = !awesome.value;
+//   isActive.value = !isActive.value;
+//   console.log("isActive", isActive);
+//   // const aaa = document.querySelector("#aaa");
+//   // if (isActive.value) {
+//   //   aaa.classList.add("active");
+//   // } else {
+//   //   aaa.classList.remove("active");
+//   // }
   
-}
-function errorToggle() {
-  hasError.value = !hasError.value;
-  // const aaa = document.querySelector("#aaa");
-  // if (hasError.value) {
-  //   aaa.classList.add("text-danger");
-  // } else {
-  //   aaa.classList.remove("text-danger");
-  // }
+// }
+// function errorToggle() {
+//   hasError.value = !hasError.value;
+//   // const aaa = document.querySelector("#aaa");
+//   // if (hasError.value) {
+//   //   aaa.classList.add("text-danger");
+//   // } else {
+//   //   aaa.classList.remove("text-danger");
+//   // }
 
-}
+// }
 </script>
 
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
-  <ComputedTodolist/><br>
-  <button @click="toggle">토글 버튼</button>
-  <button @click="errorToggle">error 토글 버튼</button>
-  <h1 v-if="awesome">Vue는 굉장해! 엄청나!</h1>
-  <h1 v-else>오 안돼 😢</h1>
+
+  <LoginForm/>
+   <!-- <ComputedTodolist/><br> -->
+  <!-- <button @click="toggle">토글 버튼</button> -->
+  <!-- <button @click="errorToggle">error 토글 버튼</button> -->
+  <!-- <h1 v-if="awesome">Vue는 굉장해! 엄청나!</h1> -->
+  <!-- <h1 v-else>오 안돼 😢</h1> -->
   <!-- div id="aaa" class="static">만세~~~</div -->
-  <div class="static" :class="{active: isActive, 'text-danger' : hasError}">만세~~~</div>
+  <!-- <div class="static" :class="{active: isActive, 'text-danger' : hasError}">만세~~~</div>  -->
 </template>
 
-<style >
-    .static {
+<!-- <style > -->
+    <!-- .static {
         border:4px solid black;
         width:200px;
         height:200px;
@@ -56,4 +61,4 @@ function errorToggle() {
         color:white;
         font-size:100px;
     }
-</style>
+</style> -->
---
+++ hello-app-vite/src/components/LoginForm.vue
@@ -0,0 +1,87 @@
+
+
+<template lang="">
+    <form v-on:submit="loginForm">
+    <p> 
+        아이디 : 
+        <input v-model = "user_id" placeholder="아이디 입력하세요"/>
+    </p>
+
+    <p> 
+        비밀번호 :
+        <input v-model = "user_pwd" placeholder="비밀번호 입력하세요"/>
+    </p>
+
+    <p> 
+        비밀번호 확인 : 
+        <input v-model = "user_pwd2" placeholder="비밀번호 입력하세요"/>
+    </p>
+
+    <div> 
+        성별: 
+        <input type="radio" id="male" value="남자" v-model="picked"/>
+        <label for="male">남자</label>
+
+        <input type="radio" id="female" value="여자" v-model="picked"/>
+        <label for="female">여자</label>
+    </div>
+
+    <div> 
+        취미: 
+        <input type="checkbox" id="drum" value="드럼" v-model="hobby">
+        <label for="drum">드럼</label>
+
+        <input type="checkbox" id="piano" value="건반" v-model="hobby">
+        <label for="piano">건반</label>
+
+        <input type="checkbox" id="guitar" value="기타" v-model="hobby">
+        <label for="guitar">기타</label>
+    </div>
+
+
+    <p> 
+        이름 : 
+        <input v-model = "user_name" placeholder="이름 입력하세요"/>
+    </p>
+
+    <p> 
+        생년월일 : 
+        <input v-model = "user_birthday" placeholder="생년월일 입력하세요"/>
+    </p>
+
+    <button @click="(event) => loginForm(event)">
+  제출하기
+</button>
+
+ 
+</form>
+</template>
+<script setup>
+    import {ref, reactive} from 'vue'
+
+    const user_id = ref('')
+    const user_pwd = ref('')
+    const user_pwd2 = ref('')
+
+    const picked = ref('')
+    const hobby = ref([])
+
+    const user_name = ref('')
+    const user_birthday = ref('')
+    
+    function loginForm(event) {
+  // 이제 네이티브 이벤트 객체에 접근할 수 있습니다.
+  if (event) {
+    event.preventDefault()
+    console.log(user_id.value)
+    alert(user_id.value)
+
+    console.log(picked.value)
+  }
+  
+}
+
+</script>
+<style lang="">
+    
+</style>
